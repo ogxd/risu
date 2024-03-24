6ae5914b187f3fa743c0730b38b542c880d05454
Olivier Giniaux <oginiaux@smartadserver.com>
2024-03-24 22:03:36 +0100

    First experimentation (not working)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # risu
+
+```shell
+brew install protobuf
+brew install grpcurl
+cargo run --bin demo
+cargo run --bin risu
+grpcurl -plaintext -import-path ./proto -proto hello.proto -d '{"name": "Tonic"}' '127.0.0.1:3001' helloworld.Greeter/SayHello
+```
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
-use std::convert::Infallible;
-use hyper::{Body, Request, Response, Server};
+use hyper::client::connect::dns::GaiResolver;
+use hyper::client::HttpConnector;
 use hyper::service::{make_service_fn, service_fn};
-
-async fn handle_request(req: Request<Body>) -> hyper::Result<Response<Body>> {
-    //let bytes = hyper::body::aggregate(req.into_body()).await?;
-    println!("Received !");
-    let bytes = hyper::body::to_bytes(req.into_body()).await?;
-    println!("Received payload: {} bytes", bytes.len());
-    Ok(Response::new(Body::from("Received")))
-}
+use hyper::{Body, Client, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    println!("Start !");
-    let make_svc = make_service_fn(|_conn| {
-        async {
-            Ok::<_, Infallible>(service_fn(handle_request))
-        }
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3001));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(forward_request))
     });
 
-    let addr = ([127, 0, 0, 1], 3100).into();
-    let server = Server::bind(&addr)
-        .http2_only(false)
-        .http2_initial_stream_window_size(5 * 1024 * 1024) // 5MB
-        .http2_initial_connection_window_size(10 * 1024 * 1024) // 10MB
-        .serve(make_svc);
+    println!("Listening on http://{}", addr);
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    Server::bind(&addr)
+        .http2_only(false) // Add missing import for the `http2` method
+        .serve(make_svc)
+        .await
+        .expect("server failed");
+}
+
+async fn forward_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let client = Client::<HttpConnector<GaiResolver>>::new();
+    let resp = client
+        .request(
+            Request::builder()
+                .uri("http://127.0.0.1:3002")
+                .body(req.into_body())
+                .unwrap(),
+        )
+        .await
+        .expect("failed to forward request");
+
+    Ok(resp)
 }
