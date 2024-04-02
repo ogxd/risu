c696e3d108abc039e8bffccd1b92ff311119b283
Olivier Giniaux <oginiaux@smartadserver.com>
2024-04-02 10:49:48 +0200

    Add std linked list to bench

--- benches/arena_linked_list.rs
+++ benches/arena_linked_list.rs
@@ -1,4 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use std::collections::LinkedList;
 use risu::ArenaLinkedList;
 
 fn benchmark(c: &mut Criterion) {
@@ -18,6 +19,26 @@ fn benchmark(c: &mut Criterion) {
         let mut list = ArenaLinkedList::new_with_capacity(4);
         let index = list.add_first(1).unwrap();
         b.iter(|| list.remove(index));
+    });
+
+    group.finish();
+
+    let mut group = c.benchmark_group(format!("LinkedList"));
+
+    group.bench_function("Add first", |b| {
+        let mut list = LinkedList::new();
+        b.iter(|| list.push_front(1));
+    });
+
+    group.bench_function("Add last", |b| {
+        let mut list = LinkedList::new();
+        b.iter(|| list.push_back(1));
+    });
+
+    group.bench_function("Remove", |b| {
+        let mut list = LinkedList::new();
+        list.push_front(1);
+        b.iter(|| list.pop_front());
     });
 
     group.finish();
